12430c53eff2997409f9d73e87a3fb8eb031f89e
luojiash <52547071@qq.com>
2016-12-02 11:01:15 +0800

    1. 修改远程仓库并跟踪分支 2. 清理已合并的分支 3. 清理远程分支

--- git.sh
+++ git.sh
@@ -8,3 +8,49 @@ for i in $(ls); do
 done
 
 # for i in $(git br | grep -v "*"); do echo $i; git lg $i --not prepare; echo "---"; done
+
+
+#修改远程仓库并跟踪分支
+change_remote() {
+    if [ -z $1 ]; then
+        echo 'usage: change_remote [new_remote]'
+        return 1
+    fi
+    addr=$(git remote -v | head -1 | awk '{print $2}' | sed "s/@.*:/@$1:/")
+    git remote rm origin
+    git remote add origin $addr
+    git fetch
+    git branch | awk '{print $NF}' |
+    while read remote; do
+        git branch -u "origin/$remote" "$remote";
+    done
+}
+
+
+# 清理已合并到$base的本地分支和远程分支
+clean() {
+    base=${1:-origin/prepare}
+    #echo $base
+    git branch | grep -v '*' | grep -v prepare | grep -v master |
+    while read br; do
+    if [ -z "`git lg $br --not $base`" ]; then
+        echo -e "\ngit branch -d $br";
+        if git branch -d $br ; then
+            echo "git push origin --delete $br";
+            git push origin --delete $br
+        fi
+    fi
+    done
+}
+
+
+clean_remote() {
+    base=${1:-origin/prepare}
+    #echo $base
+    git branch -r | grep -v ' -> ' |
+    while read br; do
+    if [ -z "`git lg $br --not $base`" ]; then
+        echo "git push origin --delete ${br#origin/}";
+    fi
+    done
+}
